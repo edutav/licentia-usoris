2ed4a82cd3f7a1bad0b3d78f9defaff9f12a8abc
Eduardo Tavares <edutav@gmail.com>
2024-11-26 21:36:45 -0400

    :sparkles: feat: Enhance seed command to include role-permission associations and user role assignments

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/edutav/licentia-usoris/infrastructure/database"
@@ -12,29 +13,31 @@ import (
 )
 
 func main() {
-	println("Hello, World!")
+	initSeed()
 }
 
-func init() {
+func initSeed() {
 	println("init")
-	// Load the configuration
+	// Load configuration
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// Establish a database connection
+	// Database connection
 	db, err := database.NewConnectionPostgres(
 		cfg.Database.Host, cfg.Database.Port, cfg.Database.User, cfg.Database.Password, cfg.Database.Name,
 	)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	defer db.Close() // Ensure the database connection is closed when the command exits
+	defer db.Close()
 
-	seedRoot(context.Background(), db)
-	seedPermissionsDefault(context.Background(), db)
-	seedRolesDefault(context.Background(), db)
+	root := seedRoot(context.Background(), db)
+	permissions := seedPermissionsDefault(context.Background(), db)
+	roles := seedRolesDefault(context.Background(), db)
+	seedRolesPermissionsDefault(context.Background(), db, roles, permissions)
+	seedUserRolesDefault(context.Background(), db, roles, root)
 }
 
 type Root struct {
@@ -45,10 +48,11 @@ type Root struct {
 	isEmailVerified bool
 	createedAt      time.Time
 	updatedAt       time.Time
+	isBlocked       bool
 	isDeleted       bool
 }
 
-func seedRoot(ctx context.Context, db *sql.DB) {
+func seedRoot(ctx context.Context, db *sql.DB) *Root {
 	// Hash the password
 	// TODO: Move password to environment variable
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("Secret@123"), bcrypt.DefaultCost)
@@ -69,6 +73,7 @@ func seedRoot(ctx context.Context, db *sql.DB) {
 		isEmailVerified: true,
 		createedAt:      time.Now().UTC(),
 		updatedAt:       time.Now().UTC(),
+		isBlocked:       false,
 		isDeleted:       false,
 	}
 
@@ -76,13 +81,14 @@ func seedRoot(ctx context.Context, db *sql.DB) {
 		INSERT INTO users (
 			name, 
 			email, 
-			password, 
+			password_hash, 
 			is_email_verified,
 			created_at,
 			updated_at,
+			is_blocked,
 			is_deleted
 		)
-		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 		RETURNING uuid
 	`
 
@@ -93,6 +99,7 @@ func seedRoot(ctx context.Context, db *sql.DB) {
 		root.isEmailVerified,
 		root.createedAt,
 		root.updatedAt,
+		root.isBlocked,
 		root.isDeleted,
 	).Scan(&root.uuid)
 
@@ -105,6 +112,8 @@ func seedRoot(ctx context.Context, db *sql.DB) {
 	}
 
 	log.Println("Root user created successfully")
+
+	return &root
 }
 
 type Permission struct {
@@ -181,7 +190,7 @@ func buildListPermissions() []*Permission {
 	}
 }
 
-func seedPermissionsDefault(ctx context.Context, db *sql.DB) {
+func seedPermissionsDefault(ctx context.Context, db *sql.DB) []*Permission {
 	println("seedPermissionsDefault")
 
 	tx, err := db.BeginTx(ctx, nil)
@@ -190,9 +199,9 @@ func seedPermissionsDefault(ctx context.Context, db *sql.DB) {
 	}
 	defer tx.Rollback()
 
-	roots := buildListPermissions()
+	permissions := buildListPermissions()
 
-	for _, root := range roots {
+	for _, permission := range permissions {
 		query := `
 			INSERT INTO permissions (
 				name,
@@ -201,7 +210,7 @@ func seedPermissionsDefault(ctx context.Context, db *sql.DB) {
 			VALUES ($1, $2)
 		`
 
-		_, err := tx.ExecContext(ctx, query, root.name, root.description)
+		_, err := tx.ExecContext(ctx, query, permission.name, permission.description)
 		if err != nil {
 			log.Fatalf("Failed to insert permission: %v", err)
 		}
@@ -212,6 +221,8 @@ func seedPermissionsDefault(ctx context.Context, db *sql.DB) {
 	}
 
 	log.Println("Permissions created successfully")
+
+	return permissions
 }
 
 type Role struct {
@@ -232,7 +243,7 @@ func buildListRoles() []*Role {
 	}
 }
 
-func seedRolesDefault(ctx context.Context, db *sql.DB) {
+func seedRolesDefault(ctx context.Context, db *sql.DB) []*Role {
 	println("seedRolesDefault")
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
@@ -262,4 +273,80 @@ func seedRolesDefault(ctx context.Context, db *sql.DB) {
 	}
 
 	log.Println("Roles created successfully")
+
+	return roles
+}
+
+func seedRolesPermissionsDefault(ctx context.Context, db *sql.DB, roles []*Role, permissions []*Permission) {
+	println("seedRolesPermissionsDefault")
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to begin transaction: %v", err)
+	}
+	defer tx.Rollback()
+
+	for _, role := range roles {
+		for _, permission := range permissions {
+			if role.name == "user" && !strings.HasPrefix(permission.name, "read:") {
+				continue
+			}
+			query := `
+				INSERT INTO role_permissions (
+					role_id,
+					permission_id
+				)
+				VALUES (
+					(SELECT uuid FROM roles WHERE name = $1),
+					(SELECT uuid FROM permissions WHERE name = $2)
+				)
+			`
+
+			_, err := tx.ExecContext(ctx, query, role.name, permission.name)
+			if err != nil {
+				log.Fatalf("Failed to insert role permission: %v", err)
+			}
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Fatalf("Failed to commit transaction: %v", err)
+	}
+
+	log.Println("Roles permissions created successfully")
+}
+
+func seedUserRolesDefault(ctx context.Context, db *sql.DB, roles []*Role, root *Root) {
+	println("seedUserRolesDefault")
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to begin transaction: %v", err)
+	}
+	defer tx.Rollback()
+
+	for _, role := range roles {
+		if root.name == "root" && role.name != "admin" {
+			continue
+		}
+		query := `
+			INSERT INTO user_roles (
+				user_id,
+				role_id
+			)
+			VALUES (
+				(SELECT uuid FROM users WHERE email = $1),
+				(SELECT uuid FROM roles WHERE name = $2)
+			)
+		`
+
+		_, err := tx.ExecContext(ctx, query, root.email, role.name)
+		if err != nil {
+			log.Fatalf("Failed to insert user role: %v", err)
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Fatalf("Failed to commit transaction: %v", err)
+	}
+
+	log.Println("User roles created successfully")
 }
